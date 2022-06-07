778f06b4145474206d2faecd8a1ef293fcf0a8be
Hrushi1509 <90896961+Hrushi1509@users.noreply.github.com>
2022-06-07 17:12:00 +0530

    rushi

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState("");
+  const handleChange = () => {
+    setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleChange}>
+        Click
+      </button>
+
+      <p id="para">{para}</p>
     </div>
   );
 }
-
 
 export default App;
